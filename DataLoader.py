bf1a807c940caf60e8608a106fe5c5c52e0a8167
mougali <Sarthak@Sarthaks-MacBook-Pro.local>
2015-11-10 15:03:13 -0600

    Added SQL Generator
    Added functions to generate SQL queries for insertion into GKA
    database. Added function calls in main().

--- DataLoader.py
+++ DataLoader.py
@@ -36,10 +36,24 @@ natKeyStandardsFile_SCD = os.getcwd()+"/Resources/NAT_KEY_SCD_STANDARD.csv"
 userGKA="AODR45CKAADVD"
 passwordGKA="ckaadvaodr5dev"
 sidGKA="CKA280D"
+connectionString=userGKA+"/"+passwordGKA+"@"+sidGKA
 
 userODS=""
 passwordODS=""
 sidODS=""
+# Command to initiate and connect to SQL*PLUS
+# sqlplus -S AODR45CKAADVD/ckaadvaodr5dev@CKA280D
+# End login information
+
+# Table information for GKA
+subTable="SUB_LOYALTYTEST"
+subDimNatKeyTable="SUB_DIM_NAT_KEY_TYP_LOYALTYTEST"
+
+# seed-file info (TEMPORARY)
+subSeedName="TMPL_SUB_DATA_20150914.csv"
+
+# Other parameters
+geolocation = "US"		# USA is default value
 
 # Standard Nat_Key_IDs
 # Sourced directly from TMPL_NAT_KEY_TYP_DATA
@@ -279,7 +293,7 @@ def update_sub_dim_nat_standards_product(standardsFile, product_NAT_ID):
 # Adds default dimension and nat-key values to TMPL_SUB_DIM_NAT_KEY_TYP_DATA
 # (PROD, PER, STORE, BAS, HHOLD, ID_CARD)
 # Record Format: SUB_ID | DIM_ID | NAT_KEY_ID | Y | N
-def add_default_sub_dim_nat_data(subDimNatDataFile, standardsFile):
+def add_default_sub_dim_nat_data(subDimNatDataFile, standardsFile, geolocation):
 
 	stdFile = open(standardsFile, 'rb')
 	outFile = open(subDimNatDataFile, 'a')
@@ -290,7 +304,12 @@ def add_default_sub_dim_nat_data(subDimNatDataFile, standardsFile):
 	# PRODUCT and PERIOD have default values (passed as parameters)
 	# Add records to SUB_DIM_NAT_KEY_TYP_DATA based on standards file
 	for row in reader:
-		if row[0] != "PROMOTION" and row[0] != "PROMODETAIL":
+		if row[0] == "PRODUCT":
+			if geolocation == "EU":
+				writer.writerow([SUB_ID,row[1],standardNatKeyDict["PROD_EU"],row[3],row[4]])
+			elif geolocation == "US":
+				writer.writerow([SUB_ID,row[1],standardNatKeyDict["PROD_US"],row[3],row[4]])
+		elif row[0] != "PROMOTION" and row[0] != "PROMODETAIL":
 			writer.writerow([SUB_ID,row[1],row[2],row[3],row[4]])
 
 	stdFile.close()
@@ -430,6 +449,83 @@ def backup_tables_GKA():
 	sql_query="create table sub_bkp_"+processDate_text+" as select * from sub;create table nat_key_typ_bkp_"+processDate_text+" as select * from nat_key_typ;create table dim_bkp_"+processDate_text+" as select * from dim;create table sub_dim_nat_key_typ_"+processDate_text+" as select * from sub_dim_nat_key_typ;"
 
 
+# Generate SQL file for GKA database
+# @param tableName
+# @param seed-file
+# return SQL query
+def createGkaQuery(tableName, seedFile):
+
+	# Call helper method
+	queryResult = getColNames(tableName)
+	headerList = queryResult.split('\n')
+	headerList.pop()	# Remove last entry (empty)
+	print headerList
+	print "\n\n\n*****************************\n"
+	print queryResult
+
+	# Logic for generating SQL script (need seed-file)
+	# 1) Open file
+	# 2) Parse through each record (except first record --> contains headers)
+	# 3) For-each record: generate SQL script
+	
+	file = open(seedFile, 'rb')
+	reader = csv.reader(file)
+
+	seedfileHeader = reader.next()		# skip header row and get number of fields in record
+	recordLength = len(seedfileHeader)
+	headerDiff = len(headerList) - recordLength
+
+	# Align seed-file fields with actual columns from table (stored in headerList)
+	# In many cases, the seed-file does not contain all required fields, and thus
+	# doesn't match up 1:1 with the CKA DB table columns (usually the missing fields are null)
+	if headerDiff > 0:
+		for i in range(0, headerDiff, 1):
+			headerList.pop()
+
+	sqlInsert = ("INSERT INTO "+tableName+" ("+', '.join(['%s']*len(headerList))+")") % tuple(headerList)
+	sqlVals = "VALUES "
+
+	fullSqlQuery = ""
+
+	for row in reader:
+		sqlVals += ("("+', '.join(['\'%s\'']*len(row))+");") % tuple(row)
+		fullSqlQuery += sqlInsert + "\n" + sqlVals + "\n\n"
+		sqlVals = "VALUES "	# RESET string
+
+	return fullSqlQuery	
+
+
+# Get column names for given table
+# Helper function for createGkaQuery
+# @param tableName
+def getColNames(tableName):
+	# Pseudocode
+	# 1) connect to GKA database
+	# 2) Use DB connection details to formulate command string
+	# 3) Issue command using subprocess.Popen(). Store stdout, stderror in vars.
+	# 4) Send SQL query to receive column-names
+
+	connectionParam=userGKA+"/"+passwordGKA+"@"+sidGKA
+
+	# Query-Flags to suppress all output other than values
+	sqlQueryFlags="SET PAGESIZE 0 FEEDBACK OFF VERIFY OFF HEADING OFF ECHO OFF\n"
+	sqlQuery=sqlQueryFlags+"SELECT column_name FROM USER_TAB_COLUMNS WHERE table_name = \'" + tableName + "\';"
+
+	queryResult, errors = runSqlQuery(sqlQuery, connectionParam)
+
+	# Returns a tuple
+	# e.g:
+	# stdout, stderror = session.communicate()
+	return queryResult
+
+#function that takes the sqlCommand and connectString and returns the queryResult and errorMessage (if any)
+# CREDIT: https://moizmuhammad.wordpress.com/2012/01/31/run-oracle-commands-from-python-via-sql-plus/
+def runSqlQuery(sqlCommand, connectString):
+   session = Popen(['sqlplus', '-S', connectString], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+   session.stdin.write(sqlCommand)
+   return session.communicate()
+
+
 def main():
 	# MAIN SCRIPT
 	# Currently able to write out standard/default values for SUBSCRIBER, OBJECT_GROUPS, and SUBJECT_AREAS
@@ -446,11 +542,9 @@ def main():
 	# TMPL_NAT_KEY_DATA, natKeyStandards, subDimNatStandards
 	productKeyBranch = raw_input("Create new product key? (Y/N) >> ")
 
-	# If user doesn't create new product key, ask which geographical
-	# region the client belongs to (currently only EU or US).
-	if productKeyBranch == "N":
-		geolocation = raw_input("Enter client geographical continent (\"EU\" or \"US\") >> ")
-
+	# Prompt user for client geographical location (impacts NAT_KEY_ID of PROD dimension)
+	geolocation = raw_input("Enter client geographical continent (\"EU\" or \"US\") >> ")
+		
 	if (productKeyBranch == "Y"):
 		# Retrieve last used ID from TMPL_NAT_KEY and use to generate newest Nat_Key_ID
 		newNatID = int(get_last_row(gkaFiles[1])[0]) + 1
@@ -460,7 +554,7 @@ def main():
 	# If NO new Product / Period key created, use original Sub-Dim-Nat Standards File
 	# Else, use newly created Standards File
 	if (subDimNatStandardsFile_New == ""):
-		add_default_sub_dim_nat_data(gkaFiles[3],subDimNatStandardsFile)
+		add_default_sub_dim_nat_data(gkaFiles[3],subDimNatStandardsFile, geolocation)
 	else: 
 		add_default_sub_dim_nat_data(gkaFiles[3],subDimNatStandardsFile_New)
 
@@ -483,13 +577,21 @@ def main():
 	# closeDeleteFiles(gkaFiles)
 	# os.rmdir(path)
 
-
 	## Steps to append to TMPL_NAT_KEY_DATA
 	# 1. Find ID of last entered record. start_id = last_ID + 1
 	# 2. Use standards file to append new default nat_keys. Keep updating currentID
 	# 3. Later modules: allow optional nat_keys
 	# ----> PROMO and PROMODETAIL are included dimensions?
 	# ----> Allow creation of new PRODUCT_GLOBAL_SCD
+
+	# SCRIPT TO OUTPUT DATA TO ODS TABLES
+	table = raw_input("Enter table-name for header file >>  ")
+	seedFile = raw_input("Enter seed-file name >> ")
+	seedFileLoc = os.getcwd()+"/Configuration Seed Files/GKA/"+seedFile
+
+	query = createGkaQuery(table, seedFileLoc)
+	print query + "\n\n"
+	# runSqlQuery(query, connectionString)
 
 if __name__ == '__main__':
 	main()
